bc4778fb04fbf25df7572bf24e06ac66924549bb
ManuelNovela <manuelnovela48@gmail.com>
2024-06-14 22:25:53 +0200

    Add JWT authentication setup

---
+++ src/main/java/com/manuelnovela/TravelAssistant/infrastructure/security/TokenService.java
@@ -0,0 +1,50 @@
+package com.manuelnovela.TravelAssistant.infrastructure.security;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import com.manuelnovela.TravelAssistant.domain.user.User;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+
+@Service
+public class TokenService {
+    @Value("api.security.token.secret")
+    private String secret;
+
+    @Value("api.sercurity.token.Issuer")
+    private String insuer;
+    public String generateToken(User user){
+        try {
+            Algorithm algorithm = Algorithm.HMAC256(secret);
+            return JWT.create()
+                    .withIssuer(insuer)
+                    .withExpiresAt(this.generanteExprireDate())
+                    .withSubject(user.getEmail())
+                    .sign(algorithm);
+        }catch (JWTCreationException exception){
+            throw new RuntimeException("Erro ao gerar o token");
+        }
+    }
+
+    public String validateToken(String token){
+        try {
+            Algorithm algorithm = Algorithm.HMAC256(secret);
+            return JWT.require(algorithm)
+                    .withIssuer(insuer)
+                    .build()
+                    .verify(token)
+                    .getSubject();
+        } catch (JWTVerificationException exception) {
+            return null;
+        }
+    }
+    private Instant generanteExprireDate (){
+        return LocalDateTime.now().plusMinutes(15).toInstant(ZoneOffset.of("02:00"));
+    }
+}
---
+++ src/main/java/com/manuelnovela/TravelAssistant/repositories/UserRepository.java
@@ -0,0 +1,11 @@
+package com.manuelnovela.TravelAssistant.repositories;
+
+import com.manuelnovela.TravelAssistant.domain.user.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, String> {
+
+    Optional<User> findByEmail(String email);
+}
