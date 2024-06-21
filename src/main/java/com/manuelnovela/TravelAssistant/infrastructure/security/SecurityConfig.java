c979218fa8459d3c0e19242f752032414581d7cd
ManuelNovela <manuelnovela48@gmail.com>
2024-06-21 05:13:08 +0200

    Update SecurityConfig.java

--- src/main/java/com/manuelnovela/TravelAssistant/infrastructure/security/SecurityConfig.java
+++ src/main/java/com/manuelnovela/TravelAssistant/infrastructure/security/SecurityConfig.java
@@ -33,6 +33,7 @@ public class SecurityConfig {
                         .requestMatchers(HttpMethod.POST, "/api/v1/auth/login").permitAll()
                         .requestMatchers(HttpMethod.POST, "/api/v1/auth/register").permitAll()
                         .requestMatchers(HttpMethod.GET, "api/v1/weather/**").permitAll()
+                        .requestMatchers(HttpMethod.GET, "api/v1/autocomplete/**").permitAll()
                         .anyRequest().authenticated()
                 )
                 .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class);
